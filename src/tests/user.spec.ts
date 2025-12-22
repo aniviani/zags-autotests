6359bc393b9fce01b13102277f379fd54b474308
Anastasiia <mac@MacBook-Pro-Nastya.local>
2025-12-22 13:14:14 +0300

    comparison of application number

--- src/tests/user.spec.ts
+++ src/tests/user.spec.ts
@@ -31,7 +31,20 @@ test.describe('User Registration Tests', () => {
     await homePage.loginAsUser();
   });
 
-  test('TC-01: Успешная регистрация брака', async () => {
+  test('TC-01: Успешная регистрация брака', async ({ page }) => {
+    
+    let apiApplicationId: string | null = null;
+    
+    page.on('response', async (response) => {
+        if (
+            response.url().includes('sendUserRequest') &&
+            response.status() === 200
+        ) {
+            const data = await response.json();
+            apiApplicationId = String(data.data.applicationid);
+        }
+    });
+
     await applicantPage.fillApplicantData(testData.applicant);
     await applicantPage.clickNext();
 
@@ -42,10 +55,14 @@ test.describe('User Registration Tests', () => {
 
     await serviceDataPage.fillMarriageData(testData.marriageService);
     await serviceDataPage.clickComplete();
-
+    
     const requestNumber = await statusPage.getRequestNumber();
+    
+    expect(apiApplicationId, 'Номер заявки не получен из API').not.toBeNull();
+    expect(requestNumber, 'Номер заявки в UI должен совпадать с API').toBe(apiApplicationId!);
+    
     expect(Number(requestNumber), 'Номер заявки должен быть больше 0 (данные с сервера получены)').toBeGreaterThan(0);
-  });
+});
 
   test('TC-02: Успешная регистрация рождения', async () => {
     await applicantPage.fillApplicantData(testData.applicant);
